959d39ffef4b63e04e7f6747d4de369c79e538c3
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-05-18 20:04:38 +0200

    Move the child component state to the ancestor

--- 01-introduction-to-react/confusion/src/App.js
+++ 01-introduction-to-react/confusion/src/App.js
@@ -2,8 +2,26 @@ import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from "./shared/dishes";
+
 
 class App extends Component {
+  /**
+   * Constructor
+   * @param props properties
+   */
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  /**
+   * All React components must
+   * implement the render() method
+   */
   render() {
     return (
       <div>
@@ -14,7 +32,8 @@ class App extends Component {
           </div>
         </Navbar>
         {/* use of a React component we've created */}
-        <Menu/>
+        {/* make this component's state available to child components via props*/}
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
--- 01-introduction-to-react/confusion/src/components/MenuComponent.js
+++ 01-introduction-to-react/confusion/src/components/MenuComponent.js
@@ -9,44 +9,8 @@ class Menu extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: [
-        {
-          id: 0,
-          name:'Uthappizza',
-          image: 'assets/images/uthappizza.png',
-          category: 'mains',
-          label:'Hot',
-          price:'4.99',
-          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-        },
-        {
-          id: 1,
-          name:'Zucchipakoda',
-          image: 'assets/images/zucchipakoda.png',
-          category: 'appetizer',
-          label:'',
-          price:'1.99',
-          description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-        },
-        {
-          id: 2,
-          name:'Vadonut',
-          image: 'assets/images/vadonut.png',
-          category: 'appetizer',
-          label:'New',
-          price:'1.99',
-          description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
-        },
-        {
-          id: 3,
-          name:'ElaiCheese Cake',
-          image: 'assets/images/elaicheesecake.png',
-          category: 'dessert',
-          label:'',
-          price:'2.99',
-          description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-        }
-      ]
+      // this component's state is supplied by the
+      // ancestor component (App in this case) via props
     };
   }
 
@@ -55,7 +19,9 @@ class Menu extends Component {
    * must implement the render() method
    */
   render() {
-    const menu = this.state.dishes.map((dish) => {
+    // no more need for using this component state as the
+    // dishes variable is handed over from the ancestor through props
+    const menu = this.props.dishes.map((dish) => {
       return (
         <div key={dish.id} className="col-12 mt-5">
           <Media tag="li">
