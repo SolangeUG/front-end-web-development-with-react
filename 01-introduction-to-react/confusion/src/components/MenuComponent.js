b519e1ad29b2ea0341f529557fbc9c222e37bcbf
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-05-18 20:24:43 +0200

    Implement response to click events

--- 01-introduction-to-react/confusion/src/components/MenuComponent.js
+++ 01-introduction-to-react/confusion/src/components/MenuComponent.js
@@ -11,7 +11,40 @@ class Menu extends Component {
     this.state = {
       // this component's state is supplied by the
       // ancestor component (App in this case) via props
+      selectedDish: null
     };
+  }
+
+  /**
+   * Update the selected dish
+   * @param dish the selected dish
+   */
+  onDishSelect(dish) {
+    // this is how we update the state of our component
+    // by calling the setState method
+    this.setState({selectedDish: dish});
+  }
+
+  /**
+   * A method to render a dish details
+   * @param dish the dish to render
+   */
+  renderDish(dish) {
+    if (dish != null) {
+      return (
+        <Card>
+          <CardImg width="100%" src={dish.image} alt={dish.name}/>
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    } else {
+      return (
+        <div></div>
+      );
+    }
   }
 
   /**
@@ -25,7 +58,7 @@ class Menu extends Component {
       return (
         <div key={dish.id} className="col-12 col-md-5 m-1">
           {/* change the way the menu items are displayed from Media to Card */}
-          <Card tag="li">
+          <Card onClick={() => this.onDishSelect(dish)}>
             <CardImg width="100%" src={dish.image} alt={dish.name}/>
             <CardImgOverlay>
               <CardTitle>{dish.name}</CardTitle>
@@ -40,6 +73,10 @@ class Menu extends Component {
       <div className="container">
         <div className="row">
           {menu}
+        </div>
+        {/* display a selected dish details */}
+        <div className="row">
+          {this.renderDish(this.state.selectedDish)}
         </div>
       </div>
     );
