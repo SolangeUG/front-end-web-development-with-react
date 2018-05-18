617ecf40ba19ebd2a6299ef7061dae211fe34517
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-05-18 16:57:41 +0200

    Configure confusion React application to use Reactstrap

--- 01-introduction-to-react/confusion/src/index.js
+++ 01-introduction-to-react/confusion/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// import Bootstrap CSS first, so that, when needed, we can
+// override it later with our own CSS specified in index.css
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
