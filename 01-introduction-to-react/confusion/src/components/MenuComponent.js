d6283097bc2d6503db6f5806a2848b82a3322e6c
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-05-18 19:16:25 +0200

    Fix the way dish descriptions are displayed

--- 01-introduction-to-react/confusion/src/components/MenuComponent.js
+++ 01-introduction-to-react/confusion/src/components/MenuComponent.js
@@ -62,10 +62,10 @@ class Menu extends Component {
             <Media left middle>
               <Media object src={dish.image} alt={dish.name}/>
             </Media>
-          </Media>
-          <Media body className="ml-5">
-            <Media heading>{dish.name}</Media>
-            <p>{dish.description}</p>
+            <Media body className="ml-5">
+              <Media heading>{dish.name}</Media>
+              <p>{dish.description}</p>
+            </Media>
           </Media>
         </div>
       );
